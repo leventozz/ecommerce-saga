92f81cbca25e643d2d9ef2ca7bacd87ac4081955
leventozz <leventufukozturk@gmail.com>
2025-11-01 13:19:45 +0300

    add orderdbcontext and orderstateinstancemap

---
+++ ECommerceSaga.Order.Infrastructure/Persistence/OrderStateDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ECommerceSaga.Order.Infrastructure.StateInstances;
+using MassTransit.EntityFrameworkCoreIntegration;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerceSaga.Order.Infrastructure.Persistence
+{
+    public class OrderStateDbContext : SagaDbContext
+    {
+        public OrderStateDbContext(DbContextOptions<OrderStateDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override IEnumerable<ISagaClassMap> Configurations
+        {
+            get { yield return new OrderStateInstanceMap(); }
+        }
+    }
+}
---
+++ ECommerceSaga.Order.Infrastructure/Persistence/OrderStateInstanceMap.cs
@@ -0,0 +1,26 @@
+﻿using ECommerceSaga.Order.Infrastructure.StateInstances;
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ECommerceSaga.Order.Infrastructure.Persistence
+{
+    public class OrderStateInstanceMap : SagaClassMap<OrderStateInstance>
+    {
+        protected override void Configure(EntityTypeBuilder<OrderStateInstance> entity, ModelBuilder model)
+        {
+            entity.Property(x => x.CurrentState)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            entity.Property(x => x.CustomerId);
+
+            entity.Property(x => x.OrderId);
+
+            entity.Property(x => x.CreatedDate);
+
+            entity.Property(x => x.FaultReason)
+                .HasMaxLength(1024);
+        }
+    }
+}
