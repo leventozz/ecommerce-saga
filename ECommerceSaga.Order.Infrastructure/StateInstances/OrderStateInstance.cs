032fd4d6aa85e38a65c64c22904e5b0b27415ca8
leventozz <leventufukozturk@gmail.com>
2025-11-01 12:22:48 +0300

    add infra and orderstateinstance

---
+++ ECommerceSaga.Order.Infrastructure/StateInstances/OrderStateInstance.cs
@@ -0,0 +1,16 @@
+﻿using MassTransit;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ECommerceSaga.Order.Infrastructure.StateInstances
+{
+    public class OrderStateInstance : SagaStateMachineInstance
+    {
+        public Guid CorrelationId { get; set; }
+        [Column(TypeName = "varchar(50)")]
+        public string CurrentState { get; set; } = string.Empty;
+        public Guid? CustomerId { get; set; }
+        public Guid? OrderId { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string FaultReason { get; set; } = string.Empty;
+    }
+}
---
+++ ECommerceSaga.Shared.Contracts/OrderSubmittedEvent.cs
@@ -0,0 +1,16 @@
+﻿namespace ECommerceSaga.Shared.Contracts
+{
+    public record OrderSubmittedEvent
+    {
+        public Guid OrderId { get; init; }
+        public Guid CustomerId { get; init; }
+        public DateTime Timestamp { get; init; }
+        public required List<OrderItem> OrderItems { get; init; }
+    }
+
+    public record OrderItem
+    {
+        public Guid ProductId { get; init; }
+        public int Quantity { get; init; }
+    }
+}
