2beec187345fb6982facd289f5e1ca64e443db19
leventozz <leventufukozturk@gmail.com>
2025-11-01 14:01:51 +0300

    add registers

---
+++ ECommerceSaga.Order.Infrastructure/DependencyInjectionExtensions.cs
@@ -0,0 +1,57 @@
+﻿using ECommerceSaga.Order.Infrastructure.Configuration;
+using ECommerceSaga.Order.Infrastructure.Persistence;
+using ECommerceSaga.Order.Infrastructure.StateInstances;
+using ECommerceSaga.Order.Infrastructure.StateMachines;
+using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace ECommerceSaga.Order.Infrastructure
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static IServiceCollection AddOrderInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("OrderStateDbConnection");
+            services.Configure<RabbitMQOptions>(configuration.GetSection(RabbitMQOptions.SectionName));
+
+            services.AddMassTransit(configurator =>
+            {
+                configurator.AddSagaStateMachine<OrderSagaStateMachine, OrderStateInstance>();
+                configurator.SetEntityFrameworkSagaRepositoryProvider(repoConfig =>
+                {
+                    // lock db row
+                    repoConfig.ConcurrencyMode = ConcurrencyMode.Pessimistic;
+
+                    repoConfig.AddDbContext<DbContext, OrderStateDbContext>((provider, options) =>
+                    {
+                        options.UseNpgsql(connectionString, pgSqlOptions =>
+                        {
+                            // for migration files
+                            pgSqlOptions.MigrationsAssembly(
+                                typeof(DependencyInjectionExtensions).Assembly.FullName);
+                        });
+                    });
+                });
+
+                configurator.UsingRabbitMq((context, mqConfig) =>
+                {
+                    var rabbitMQOptions = context.GetRequiredService<IOptions<RabbitMQOptions>>().Value;
+
+                    mqConfig.Host(rabbitMQOptions.HostName, "/", h =>
+                    {
+                        h.Username(rabbitMQOptions.UserName);
+                        h.Password(rabbitMQOptions.Password);
+                    });
+                    mqConfig.ConfigureEndpoints(context);
+                });
+            });
+
+            return services;
+        }
+    }
+}
---
+++ ECommerceSaga.Order.Infrastructure/StateMachines/OrderSagaStateMachine.cs
@@ -0,0 +1,51 @@
+﻿using ECommerceSaga.Order.Infrastructure.StateInstances;
+using ECommerceSaga.Shared.Contracts;
+using MassTransit;
+
+namespace ECommerceSaga.Order.Infrastructure.StateMachines
+{
+    public class OrderSagaStateMachine : MassTransitStateMachine<OrderStateInstance>
+    {
+        #region States
+
+        public State Submitted { get; private set; }
+        public State AwaitingInventory { get; private set; }
+        public State AwaitingPayment { get; private set; }
+        public State Completed { get; private set; }
+        public State Faulted { get; private set; } //error
+        public State Cancelled { get; private set; }
+
+        #endregion
+
+        #region Events
+        public Event<OrderSubmittedEvent> OrderSubmitted { get; private set; }
+
+        //later
+        // public Event<InventoryReservedEvent> InventoryReserved { get; private set; }
+        // public Event<InventoryReservationFailedEvent> InventoryReservationFailed { get; private set; }
+        // public Event<PaymentCompletedEvent> PaymentCompleted { get; private set; }
+        // public Event<PaymentFailedEvent> PaymentFailed { get; private set; }
+
+        #endregion
+
+        #region Constructor
+
+        public OrderSagaStateMachine()
+        {
+            InstanceState(x => x.CurrentState);
+
+            Initially(
+                When(OrderSubmitted)
+                    .Then(context =>
+                    {
+                        context.Saga.OrderId = context.Message.OrderId;
+                        context.Saga.CustomerId = context.Message.CustomerId;
+                        context.Saga.CreatedDate = context.Message.Timestamp;
+                    })
+                    .TransitionTo(Submitted)
+            );
+        }
+
+        #endregion
+    }
+}
