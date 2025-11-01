48f62b74d0b945c16eb2c201ad7c111f7c2629d5
leventozz <leventufukozturk@gmail.com>
2025-11-01 14:16:20 +0300

    add first migration

---
+++ ECommerceSaga.Order.Infrastructure/Migrations/20251101111105_InitialOrderStateSchema.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommerceSaga.Order.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialOrderStateSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OrderStateInstance",
+                columns: table => new
+                {
+                    CorrelationId = table.Column<Guid>(type: "uuid", nullable: false),
+                    CurrentState = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false),
+                    CustomerId = table.Column<Guid>(type: "uuid", nullable: true),
+                    OrderId = table.Column<Guid>(type: "uuid", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    FaultReason = table.Column<string>(type: "character varying(1024)", maxLength: 1024, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderStateInstance", x => x.CorrelationId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderStateInstance");
+        }
+    }
+}
