d3e6b49d91e1c63d7e33d407488bec8665179391
leventozz <leventufukozturk@gmail.com>
2025-11-05 21:01:36 +0300

    add missing command

---
+++ ECommerceSaga.Shared.Contracts/Inventory/ReleaseInventoryCommand.cs
@@ -0,0 +1,10 @@
+﻿using ECommerceSaga.Shared.Contracts.Common;
+
+namespace ECommerceSaga.Shared.Contracts.Inventory
+{
+    public class ReleaseInventoryCommand
+    {
+        public Guid CorrelationId { get; init; }
+        public required List<OrderItem> OrderItems { get; init; }
+    }
+}
